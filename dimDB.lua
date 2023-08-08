959477b17c399d50dd706dd4158cf2c0742e31c6
liz-lovelace <ivanov0604041@gmail.com>
2023-08-09 00:46:56 +0600

    db backend

---
+++ dimDB.lua
@@ -0,0 +1,88 @@
+local json = require("dkjson")
+
+local function readDB()
+    local f = io.open("dim.json", "r")
+    if not f then
+        return {}
+    end
+
+    local content = f:read("*all")
+    f:close()
+
+    local status, result = pcall(json.decode, content)
+    if status then
+        return result
+    else
+        return {}
+    end
+end
+
+local function writeDB(db)
+    local f = io.open("dim.json", "w")
+    if not f then
+        print("Error: Could not write to 'dim.json'")
+        return
+    end
+
+    f:write(json.encode(db))
+    f:close()
+end
+
+local function connect(file1, file2)
+    if file1 == file2 then
+        return "Failed: Files are identical"
+    end
+
+    local pair = {file1, file2}
+    table.sort(pair)
+
+    local db = readDB()
+
+    for _, v in ipairs(db) do
+        if v[1] == pair[1] and v[2] == pair[2] then
+            return "Failed: Connection already exists"
+        end
+    end
+
+    table.insert(db, pair)
+    writeDB(db)
+    return "Success: Files connected"
+end
+
+local function disconnect(file1, file2)
+    local pair = {file1, file2}
+    table.sort(pair)
+
+    local db = readDB()
+
+    for i, v in ipairs(db) do
+        if v[1] == pair[1] and v[2] == pair[2] then
+            table.remove(db, i)
+            writeDB(db)
+            return "Success: Files disconnected"
+        end
+    end
+    return "Failure: Files to disconnect not found"
+end
+
+local function connectedFiles(file)
+    local db = readDB()
+    local connected = {}
+
+    for _, v in ipairs(db) do
+        if v[1] == file then
+            table.insert(connected, v[2])
+        elseif v[2] == file then
+            table.insert(connected, v[1])
+        end
+    end
+
+    return connected
+end
+
+return {
+    connect = connect,
+    disconnect = disconnect,
+    connectedFiles = connectedFiles,
+}
+
--- main.lua
+++ main.lua
@@ -1,17 +1,41 @@
+local db = require("dimDB")
 require("editor_bridge")
 
 function love.load()
   love.keyboard.setKeyRepeat( true )
-  font = love.graphics.newFont('iosevka-splendid-regular.ttf', 12)
+  font = love.graphics.newFont('iosevka-splendid-regular.ttf', 14)
   love.graphics.setFont(font)
   
   run_editor()
 end
 
 function love.draw()
-  love.graphics.clear(0.5, 0, 0)
+  love.graphics.clear(0, 0, 0)
+
+  local connectedFiles = db.connectedFiles("d.note")
+  love.graphics.setColor(0, 1, 1)
+  local y = 0
+  for i, file in pairs(connectedFiles) do
+    love.graphics.print(i..file, 400, y)
+    y = y + 20
+  end
+
   local pane_output = grab_editor_output()
-  love.graphics.print(pane_output.content, 0, 0)
+  love.graphics.setColor(0, 1, 0)
+  local y = 0
+  love.graphics.setColor(0, 0, 1)
+	love.graphics.rectangle("fill", pane_output.cursor.x * 7,pane_output.cursor.y * 18, 7,18)
+  for line in pane_output.content:gmatch("[^\n]+") do
+    local x = 0
+    for char in line:gmatch('.') do
+      love.graphics.setColor(0, 1, 0)
+      love.graphics.print(char, x, y)
+      x = x + 7
+    end
+    love.graphics.setColor(0, 0.1, 0)
+    love.graphics.line(0,y, 600,y)
+    y = y + 18
+  end
   love.graphics.print(string.format("FPS %d", love.timer.getFPS( )), love.graphics.getWidth() - 40, 0)
   love.graphics.print(string.format("cursor %d %d", pane_output.cursor.x, pane_output.cursor.y), love.graphics.getWidth() - 80, 20)
   
